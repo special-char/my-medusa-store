d8fdda9757e88ec9ce8704d04fea69d3b5bb2ece
Korat Dishant <koratdishant536631@gmail.com>
2023-11-24 09:37:52 +0530

    commit

--- src/admin/routes/analytics/page.tsx
+++ src/admin/routes/analytics/page.tsx
@@ -2,7 +2,14 @@ import { RouteConfig } from "@medusajs/admin";
 import { RocketLaunch } from "@medusajs/icons";
 
 const AnalyticsPage = () => {
-  return <div>This is my custom route</div>;
+  return <div>
+    
+    
+    
+    This is my custom route
+    
+    
+    charts</div>;
 };
 
 export const config: RouteConfig = {
